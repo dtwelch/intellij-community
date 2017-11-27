3daca35bfeaf0378ede6e967b2a4c9a4b4e3cc5d
dtwelch <dtw.welch@gmail.com>
2017-11-27 16:55:57 -0500

    tweak clone commands for android dependencies for next time (if there is a next time :)

--- getPlugins.sh
+++ getPlugins.sh
@@ -1,2 +1,5 @@
-git clone git://git.jetbrains.org/idea/android.git android
-git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
+git clone https://github.com/dtwelch/android.git android 
+git clone https://github.com/dtwelch/adt-tools-base.git android/tools-base
+
+#git clone git://git.jetbrains.org/idea/android.git android
+#git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
