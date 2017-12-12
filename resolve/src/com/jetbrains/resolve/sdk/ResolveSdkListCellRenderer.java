3b74c16bc1e26480a69a542ed01b39c0e0d8e7ea
Dan Welch <dtw.welch@gmail.com>
2017-12-12 08:30:44 -0500

    added more to the list cell renderer

--- resolve/src/com/jetbrains/resolve/sdk/ResolveSdkListCellRenderer.java
+++ resolve/src/com/jetbrains/resolve/sdk/ResolveSdkListCellRenderer.java
@@ -2,9 +2,11 @@
 package com.jetbrains.resolve.sdk;
 
 import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.SdkType;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.ui.ColoredListCellRenderer;
+import com.intellij.ui.SimpleTextAttributes;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
@@ -15,18 +17,23 @@ public class ResolveSdkListCellRenderer extends ColoredListCellRenderer<Object> 
     if (value instanceof Sdk) {
       Sdk valueAsSdk = (Sdk) value;
       appendName(valueAsSdk);
-      setIcon(customizeIcon(valueAsSdk));
+      if (valueAsSdk.getSdkType() instanceof SdkType) {
+        setIcon(((SdkType)valueAsSdk.getSdkType()).getIcon());
+      }
     }
     else {
-
+      append("<No SDK>");
     }
   }
 
   private void appendName(@NotNull Sdk sdk) {
+    String name = sdk.getName();
+    append(name);
+    String homePath = sdk.getHomePath();
+    String relHomePath = homePath != null ? FileUtil.getLocationRelativeToUserHome(homePath) : null;
+    if (relHomePath != null) {
+      append(" (" + relHomePath + ")", SimpleTextAttributes.GRAYED_ATTRIBUTES);
+    }
   }
 
-  @Nullable
-  private Icon customizeIcon(@NotNull Sdk sdk) {
-    return null;
-  }
 }
