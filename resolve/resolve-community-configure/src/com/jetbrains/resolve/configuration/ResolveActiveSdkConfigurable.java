4e0d10c1068704ab84668dbe319477f2b7ebb024
Dan Welch <dtw.welch@gmail.com>
2018-01-08 19:10:10 -0500

    added more

--- resolve/resolve-community-configure/src/com/jetbrains/resolve/configuration/ResolveActiveSdkConfigurable.java
+++ resolve/resolve-community-configure/src/com/jetbrains/resolve/configuration/ResolveActiveSdkConfigurable.java
@@ -1,4 +1,27 @@
 package com.jetbrains.resolve.configuration;
 
-public class ResolveActiveSdkConfigurable {
+import com.intellij.openapi.options.UnnamedConfigurable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel;
+import com.intellij.ui.ComboboxWithBrowseButton;
+import com.intellij.util.NullableConsumer;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.util.Set;
+
+public class ResolveActiveSdkConfigurable extends UnnamedConfigurable {
+
+  @NotNull private final Project myProject;
+  private MySdkModelListener mySdkModelListener = new MySdkModelListener();
+  private ResolveConfigurableCompilerList myCompilerList;
+  private ProjectSdksModel myProjectSdksModel;
+  private NullableConsumer<Sdk> myAddSdkCallback;
+  private boolean mySdkSettingsWereModified = false;
+
+  private JPanel myMainPanel;
+  private ComboboxWithBrowseButton mySdkCombo;
+  private Set<Sdk> myInitialSdkSet;
+
 }
